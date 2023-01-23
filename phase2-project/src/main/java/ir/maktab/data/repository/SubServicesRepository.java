7ec4e25f488af52b2810741f5c24efacc8dc8e32
kosar khodadadi <kosar.khodadadi999@gmail.com>
2023-01-23 16:57:12 +0330

    creat SubServicesRepository interface

---
+++ phase2-project/src/main/java/ir/maktab/data/repository/SubServicesRepository.java
@@ -0,0 +1,7 @@
+package ir.maktab.data.repository;
+
+import ir.maktab.data.model.SubServices;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SubServicesRepository extends JpaRepository<SubServices, Long> {
+}
