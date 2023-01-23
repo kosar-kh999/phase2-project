2a83a905ec5f48a68bb4478a2a1014726d4c6a50
kosar khodadadi <kosar.khodadadi999@gmail.com>
2023-01-24 00:04:58 +0330

    add custom method in ExpertRepository interface

--- phase2-project/src/main/java/ir/maktab/data/repository/ExpertRepository.java
+++ phase2-project/src/main/java/ir/maktab/data/repository/ExpertRepository.java
@@ -1,7 +1,13 @@
 package ir.maktab.data.repository;
 
+import ir.maktab.data.enums.ExpertStatus;
 import ir.maktab.data.model.Expert;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ExpertRepository extends JpaRepository<Expert, Long> {
+    Optional<Expert> findExpertByEmail(String email);
+
+    Expert getExpertByExpertStatus(ExpertStatus expertStatus);
 }
