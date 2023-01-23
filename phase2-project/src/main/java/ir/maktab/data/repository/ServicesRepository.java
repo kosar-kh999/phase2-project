8ca3e225a4739fb608b8bb7e5865705a9493794e
kosar khodadadi <kosar.khodadadi999@gmail.com>
2023-01-23 16:56:59 +0330

    creat ServicesRepository interface

---
+++ phase2-project/src/main/java/ir/maktab/data/repository/ServicesRepository.java
@@ -0,0 +1,7 @@
+package ir.maktab.data.repository;
+
+import ir.maktab.data.model.Services;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ServicesRepository extends JpaRepository<Services, Long> {
+}
