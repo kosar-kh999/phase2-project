1a97748a71bceaac3c215419c384c5c08700372f
kosar khodadadi <kosar.khodadadi999@gmail.com>
2023-01-25 00:54:44 +0330

    ServicesRepository creat custom method

--- phase2-project/src/main/java/ir/maktab/data/repository/ServicesRepository.java
+++ phase2-project/src/main/java/ir/maktab/data/repository/ServicesRepository.java
@@ -3,5 +3,8 @@ package ir.maktab.data.repository;
 import ir.maktab.data.model.Services;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ServicesRepository extends JpaRepository<Services, Long> {
+    Optional<Services> findByName(String name);
 }
