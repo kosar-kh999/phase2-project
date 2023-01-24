afa17edf2a55384b4c036b3c50739a17b8c7a536
kosar khodadadi <kosar.khodadadi999@gmail.com>
2023-01-25 00:55:24 +0330

    SubServicesRepository add custom method

--- phase2-project/src/main/java/ir/maktab/data/repository/SubServicesRepository.java
+++ phase2-project/src/main/java/ir/maktab/data/repository/SubServicesRepository.java
@@ -2,6 +2,20 @@ package ir.maktab.data.repository;
 
 import ir.maktab.data.model.SubServices;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface SubServicesRepository extends JpaRepository<SubServices, Long> {
+    Optional<SubServices> findBySubName(String name);
+
+    @Modifying
+    @Query("update SubServices s set s.briefExplanation=:brief where s.subName=:sub")
+    void updateBrief(@Param("brief") String brief, @Param("sub") String sub);
+
+    @Modifying
+    @Query("update SubServices s set s.price=:price where s.subName=:sub")
+    void updatePrice(@Param("price") double price, @Param("sub") String sub);
 }
