9a12b520fe086c781dc761a7e331cdcb64c34279
kosar khodadadi <kosar.khodadadi999@gmail.com>
2023-01-24 12:51:28 +0330

    add crud method ServicesService class

--- phase2-project/src/main/java/ir/maktab/service/ServicesService.java
+++ phase2-project/src/main/java/ir/maktab/service/ServicesService.java
@@ -1,9 +1,30 @@
 package ir.maktab.service;
 
+import ir.maktab.data.model.Services;
+import ir.maktab.data.repository.ServicesRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class ServicesService {
+    private final ServicesRepository servicesRepository;
+
+    public void addServices(Services services) {
+        servicesRepository.save(services);
+    }
+
+    public List<Services> getAllServices() {
+        return servicesRepository.findAll();
+    }
+
+    public void updateServices(Services services) {
+        servicesRepository.save(services);
+    }
+
+    public void deleteServices(Services services) {
+        servicesRepository.delete(services);
+    }
 }
