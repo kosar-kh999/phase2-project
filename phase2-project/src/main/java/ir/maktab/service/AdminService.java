ebd68fb763e23a254a05ef4ea097dea8686af282
kosar khodadadi <kosar.khodadadi999@gmail.com>
2023-01-25 00:54:08 +0330

    AdminService add new method

--- phase2-project/src/main/java/ir/maktab/service/AdminService.java
+++ phase2-project/src/main/java/ir/maktab/service/AdminService.java
@@ -1,9 +1,67 @@
 package ir.maktab.service;
 
+import ir.maktab.data.enums.ExpertStatus;
+import ir.maktab.data.model.Expert;
+import ir.maktab.data.model.Services;
+import ir.maktab.data.model.SubServices;
+import ir.maktab.util.exception.NotFound;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class AdminService {
+
+    private final ServicesService servicesService;
+    private final SubServicesService subServicesService;
+    private final ExpertService expertService;
+
+    public void addServices(String name) throws NotFound {
+        Services serviceByName = servicesService.getByName(name);
+        servicesService.addServices(serviceByName);
+    }
+
+    public void addSubService(String name, String subName) throws NotFound {
+        servicesService.getByName(name);
+        SubServices subServicesByName = subServicesService.getByName(subName);
+        subServicesService.saveSubService(subServicesByName);
+    }
+
+    public Expert addExpertToSubService(Expert expert, String name) throws NotFound {
+        SubServices subServicesByName = subServicesService.getByName(name);
+        expert.getSubServices().add(subServicesByName);
+        expertService.update(expert);
+        return expert;
+    }
+
+    public void deleteExpertFromSubServices(Expert expert, SubServices subServices) throws NotFound {
+        if (!(expert.getSubServices().contains(subServices)))
+            throw new NotFound("not found this sub service for this expert");
+        expert.getSubServices().remove(subServices);
+        expertService.update(expert);
+    }
+
+    public List<Services> showAllServices() {
+        return servicesService.getAllServices();
+    }
+
+    public SubServices updatePriceAndBrief(String name, double price, String brief) throws NotFound {
+        SubServices subServiceByName = subServicesService.getByName(name);
+        subServiceByName.setPrice(price);
+        subServiceByName.setBriefExplanation(brief);
+        subServicesService.updateSubServices(subServiceByName);
+        return subServiceByName;
+    }
+
+    public Expert editStatus(Expert expert) {
+        Expert expertStatus = expertService.getStatus(expert.getExpertStatus());
+        if (expertStatus.getExpertStatus().equals(ExpertStatus.NEW)) {
+            expert.setExpertStatus(ExpertStatus.CONFIRMED);
+            expertService.update(expert);
+        }
+        return expert;
+
+    }
 }
