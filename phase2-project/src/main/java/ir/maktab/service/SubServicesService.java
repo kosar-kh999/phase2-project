b91932ca3ef3a60c39e237cdeb70a0e8f67c373a
kosar khodadadi <kosar.khodadadi999@gmail.com>
2023-01-25 00:55:41 +0330

    SubServicesService add new method

--- phase2-project/src/main/java/ir/maktab/service/SubServicesService.java
+++ phase2-project/src/main/java/ir/maktab/service/SubServicesService.java
@@ -2,6 +2,7 @@ package ir.maktab.service;
 
 import ir.maktab.data.model.SubServices;
 import ir.maktab.data.repository.SubServicesRepository;
+import ir.maktab.util.exception.NotFound;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -27,5 +28,17 @@ public class SubServicesService {
 
     public void deleteSubServices(SubServices subServices) {
         subServicesRepository.delete(subServices);
+    }
+
+    public SubServices getByName(String name) throws NotFound {
+        return subServicesRepository.findBySubName(name).orElseThrow(() -> new NotFound("This email is not exist ! "));
+    }
+
+    public void updatePrice(double price, String name) {
+        subServicesRepository.updatePrice(price, name);
+    }
+
+    public void updateBrief(String brief, String name) {
+        subServicesRepository.updateBrief(brief, name);
     }
 }
