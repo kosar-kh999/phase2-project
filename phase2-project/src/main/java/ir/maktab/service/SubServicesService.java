b6c74113ab84f40b9859b7a301fa902cb6e077de
kosar khodadadi <kosar.khodadadi999@gmail.com>
2023-01-24 12:51:43 +0330

    add crud method SubServicesService class

--- phase2-project/src/main/java/ir/maktab/service/SubServicesService.java
+++ phase2-project/src/main/java/ir/maktab/service/SubServicesService.java
@@ -1,9 +1,31 @@
 package ir.maktab.service;
 
+import ir.maktab.data.model.SubServices;
+import ir.maktab.data.repository.SubServicesRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class SubServicesService {
+
+    private final SubServicesRepository subServicesRepository;
+
+    public void saveSubService(SubServices subServices) {
+        subServicesRepository.save(subServices);
+    }
+
+    public List<SubServices> getAllSubServices() {
+        return subServicesRepository.findAll();
+    }
+
+    public void updateSubServices(SubServices subServices) {
+        subServicesRepository.save(subServices);
+    }
+
+    public void deleteSubServices(SubServices subServices) {
+        subServicesRepository.delete(subServices);
+    }
 }
