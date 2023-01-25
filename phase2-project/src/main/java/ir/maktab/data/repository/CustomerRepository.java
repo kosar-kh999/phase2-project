2b4f0748ee6660fd3c85f87959b282ef9f352e2c
kosar khodadadi <kosar.khodadadi999@gmail.com>
2023-01-25 17:29:07 +0330

    CustomerRepository added

--- phase2-project/src/main/java/ir/maktab/data/repository/CustomerRepository.java
+++ phase2-project/src/main/java/ir/maktab/data/repository/CustomerRepository.java
@@ -3,5 +3,8 @@ package ir.maktab.data.repository;
 import ir.maktab.data.model.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
+    Optional<Customer> findCustomerByEmail(String email);
 }
