3ea73c2c05a78774b5600898f5d2916676b31280
kosar khodadadi <kosar.khodadadi999@gmail.com>
2023-01-26 00:48:00 +0330

    SuggestionRepository interface define

--- phase2-project/src/main/java/ir/maktab/data/repository/SuggestionRepository.java
+++ phase2-project/src/main/java/ir/maktab/data/repository/SuggestionRepository.java
@@ -1,7 +1,13 @@
 package ir.maktab.data.repository;
 
+import ir.maktab.data.model.OrderSystem;
 import ir.maktab.data.model.Suggestion;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface SuggestionRepository extends JpaRepository<Suggestion, Long> {
+    List<Suggestion> findByOrderSystemOrderByPrice(OrderSystem orderSystem);
+
+
 }
