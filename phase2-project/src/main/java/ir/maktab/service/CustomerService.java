84f96e7d709a1a0dac27d5fe1028ddd5d583876e
kosar khodadadi <kosar.khodadadi999@gmail.com>
2023-01-25 17:29:25 +0330

    CustomerService add new method

--- phase2-project/src/main/java/ir/maktab/service/CustomerService.java
+++ phase2-project/src/main/java/ir/maktab/service/CustomerService.java
@@ -1,9 +1,57 @@
 package ir.maktab.service;
 
+import ir.maktab.data.model.Customer;
+import ir.maktab.data.repository.CustomerRepository;
+import ir.maktab.util.exception.NotCorrect;
+import ir.maktab.util.exception.NotFoundUser;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class CustomerService {
+
+    private final CustomerRepository customerRepository;
+    private final ServicesService servicesService;
+    private final SubServicesService subServicesService;
+
+    public void signUp(Customer customer) {
+        customerRepository.save(customer);
+    }
+
+    public Customer signIn(String email, String password) throws NotFoundUser {
+        Customer customer = customerRepository.findCustomerByEmail(email).orElseThrow(() -> new NotFoundUser("This email is not exist ! "));
+        if (!(customer.getPassword().equals(password)))
+            throw new NotFoundUser("This user is not correct");
+        return customer;
+    }
+
+    public void changePassword(String newPassword, String confirmedPassword, Customer customer) throws NotCorrect {
+        if (!newPassword.equals(confirmedPassword))
+            throw new NotCorrect("The new password and confirmed password must be match");
+        customer.setPassword(newPassword);
+        customerRepository.save(customer);
+    }
+
+    public List<Customer> getAll() {
+        return customerRepository.findAll();
+    }
+
+    public void delete(Customer customer) {
+        customerRepository.delete(customer);
+    }
+
+    public void update(Customer customer) {
+        customerRepository.save(customer);
+    }
+
+    public void showAllServices() {
+        servicesService.getAllServices();
+    }
+
+    public void showAllSubServices() {
+        subServicesService.getAllSubServices();
+    }
 }
