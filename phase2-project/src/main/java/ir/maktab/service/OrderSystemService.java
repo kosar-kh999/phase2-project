8c1166e28acc3d2e34a8912c69f391bda4e058d5
kosar khodadadi <kosar.khodadadi999@gmail.com>
2023-01-29 01:49:36 +0330

    OrderSystemService edit methods

--- phase2-project/src/main/java/ir/maktab/service/OrderSystemService.java
+++ phase2-project/src/main/java/ir/maktab/service/OrderSystemService.java
@@ -2,20 +2,16 @@ package ir.maktab.service;
 
 import ir.maktab.data.enums.OrderStatus;
 import ir.maktab.data.model.OrderSystem;
-import ir.maktab.data.model.SubServices;
 import ir.maktab.data.repository.OrderSystemRepository;
-import ir.maktab.util.exception.NotFound;
 import ir.maktab.util.exception.OrderException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-
-import java.util.Date;
 
 @Service
 @RequiredArgsConstructor
 public class OrderSystemService {
     private final OrderSystemRepository orderSystemRepository;
-    private final ServicesService servicesService;
+    private final MainServicesService mainServicesService;
     private final SubServicesService subServicesService;
 
     public void addOrder(OrderSystem orderSystem) {
