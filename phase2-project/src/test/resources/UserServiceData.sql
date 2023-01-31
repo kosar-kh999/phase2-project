bfadac55773078af6c99261c511f43699d8f13b4
kosar khodadadi <kosar.khodadadi999@gmail.com>
2023-01-31 14:38:07 +0330

    not used

--- phase2-project/src/test/resources/UserServiceData.sql
+++ phase2-project/src/test/resources/UserServiceData.sql
@@ -1,1 +1,6 @@
-
+/*insert into expert (id, credit, email, entry_date, first_name, last_name, password, role, expert_status, image,
+                    is_confirmed, score)
+values (1,100000,'babak.vahidi@gmail.com','2023.2.1 00:00:00','babak','vahidi','111cccXX','EXPORT','NEW',false,3);
+insert into expert (id, credit, email, entry_date, first_name, last_name, password, role, expert_status, image,
+                    is_confirmed, score)
+values (2,200000,'babak.vahidi@gmail.com','2023.2.1 00:00:00','babak','vahidi','111cccXX','EXPORT','NEW',false,4);*/
