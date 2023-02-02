951b6e115d15ce1075bafb20b4570cea2aa7c8b4
kosar khodadadi <kosar.khodadadi999@gmail.com>
2023-02-03 01:07:57 +0330

    NotCorrect exception to runtime exception

--- phase2-project/src/main/java/ir/maktab/util/exception/NotCorrect.java
+++ phase2-project/src/main/java/ir/maktab/util/exception/NotCorrect.java
@@ -1,6 +1,6 @@
 package ir.maktab.util.exception;
 
-public class NotCorrect extends Exception {
+public class NotCorrect extends RuntimeException {
     public NotCorrect(String message) {
         super(message);
     }
