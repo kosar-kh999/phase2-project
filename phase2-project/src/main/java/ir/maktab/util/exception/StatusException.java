9e2a515ecd1303c6ea2ac0db3322a79095037217
kosar khodadadi <kosar.khodadadi999@gmail.com>
2023-02-03 01:10:35 +0330

    StatusException exception to runtime exception

--- phase2-project/src/main/java/ir/maktab/util/exception/StatusException.java
+++ phase2-project/src/main/java/ir/maktab/util/exception/StatusException.java
@@ -1,6 +1,6 @@
 package ir.maktab.util.exception;
 
-public class StatusException extends Exception {
+public class StatusException extends RuntimeException {
     public StatusException(String message) {
         super(message);
     }
