e0e9050ce3896eba09c8aab95e30325d7e2abb13
kosar khodadadi <kosar.khodadadi999@gmail.com>
2023-02-03 01:09:39 +0330

    OrderSystemService exception to runtime exception

--- phase2-project/src/main/java/ir/maktab/service/OrderSystemService.java
+++ phase2-project/src/main/java/ir/maktab/service/OrderSystemService.java
@@ -3,7 +3,6 @@ package ir.maktab.service;
 import ir.maktab.data.enums.OrderStatus;
 import ir.maktab.data.model.OrderSystem;
 import ir.maktab.data.model.SubServices;
-import ir.maktab.data.model.Suggestion;
 import ir.maktab.data.repository.OrderSystemRepository;
 import ir.maktab.util.exception.NotFound;
 import ir.maktab.util.exception.OrderException;
@@ -21,12 +20,13 @@ public class OrderSystemService {
     public void addOrder(OrderSystem orderSystem) {
         orderSystemRepository.save(orderSystem);
     }
-    public OrderSystem getSuggestionById(Long id) throws NotFound {
-        return orderSystemRepository.findOrderSystemById(id).orElseThrow(()->new NotFound("Not found this order"));
+
+    public OrderSystem getSuggestionById(Long id) {
+        return orderSystemRepository.findOrderSystemById(id).orElseThrow(() -> new NotFound("Not found this order"));
     }
 
     @Transactional
-    public OrderSystem addOrderWithSubService(SubServices subServices, OrderSystem orderSystem) throws OrderException {
+    public OrderSystem addOrderWithSubService(SubServices subServices, OrderSystem orderSystem) {
         if (orderSystem.getPrice() < subServices.getPrice())
             throw new OrderException("Price must be more than original");
         if (new Date().after(orderSystem.getTimeToDo()))
@@ -35,13 +35,13 @@ public class OrderSystemService {
         return orderSystemRepository.save(orderSystem);
     }
 
-    public void showOrderToExpert(OrderSystem orderSystem) throws OrderException {
+    public void showOrderToExpert(OrderSystem orderSystem)  {
         if (!(orderSystem.getOrderStatus().equals(OrderStatus.WAITING_EXPERT_SELECTION) || orderSystem.getOrderStatus().
                 equals(OrderStatus.WAITING_ADVICE_EXPERTS)))
             throw new OrderException("the order status must be WAITING_EXPERT_SELECTION or WAITING_ADVICE_EXPERTS");
     }
 
-    public void changeOrderStatus(OrderSystem orderSystem){
+    public void changeOrderStatus(OrderSystem orderSystem) {
         orderSystem.setOrderStatus(OrderStatus.WAITING_EXPERT_SELECTION);
         orderSystemRepository.save(orderSystem);
     }
