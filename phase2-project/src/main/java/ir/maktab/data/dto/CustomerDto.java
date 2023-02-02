b3b52d9564543397b95d3c027167dbb110a95b8d
kosar khodadadi <kosar.khodadadi999@gmail.com>
2023-02-03 01:03:44 +0330

    CustomerDto entity define for controller

---
+++ phase2-project/src/main/java/ir/maktab/data/dto/CustomerDto.java
@@ -0,0 +1,33 @@
+package ir.maktab.data.dto;
+
+import ir.maktab.data.enums.Role;
+import jakarta.validation.constraints.Pattern;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class CustomerDto {
+
+    @Pattern(regexp = "^[A-Za-z]\\w{2,29}$")
+    private String firstName;
+    @Pattern(regexp = "^[A-Za-z]\\w{2,29}$")
+    private String lastName;
+    @Pattern(regexp = "^[a-zA-Z0-9_+&*-]+(?:\\." +
+            "[a-zA-Z0-9_+&*-]+)*@" +
+            "(?:[a-zA-Z0-9-]+\\.)+[a-z" +
+            "A-Z]{2,7}$")
+    private String email;
+    private Role role;
+    @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z]).{8}$")
+    private String password;
+    private Date entryDate;
+    private double credit;
+    private List<OrderSystemDto> orderSystemDto = new ArrayList<>();
+}
