a53fa4d1d729f9dc427eab25cb1536a376726f8b
kosar khodadadi <kosar.khodadadi999@gmail.com>
2023-02-03 01:08:36 +0330

    OrderException exception to runtime exception

--- phase2-project/src/main/java/ir/maktab/util/exception/OrderException.java
+++ phase2-project/src/main/java/ir/maktab/util/exception/OrderException.java
@@ -1,6 +1,6 @@
 package ir.maktab.util.exception;
 
-public class OrderException extends Exception {
+public class OrderException extends RuntimeException {
     public OrderException(String message) {
         super(message);
     }
