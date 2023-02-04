81a21102b19277a676a646c2ed83e8a7a49f5273
kosar khodadadi <kosar.khodadadi999@gmail.com>
2023-02-04 19:20:09 +0330

    SubServiceDtoDto changes for controller

--- phase2-project/src/main/java/ir/maktab/data/dto/SubServiceDto.java
+++ phase2-project/src/main/java/ir/maktab/data/dto/SubServiceDto.java
@@ -1,11 +1,14 @@
 package ir.maktab.data.dto;
 
-import ir.maktab.data.model.MainService;
-import jakarta.persistence.ManyToOne;
+import lombok.*;
 
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class SubServiceDto {
-    @ManyToOne
-    private MainService mainService;
+
     private String subName;
     private double price;
     private String briefExplanation;
